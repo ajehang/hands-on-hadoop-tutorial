b4c4b8866b4d1a0829757d2c824a3eeae35e14b3
parallels <parallels@parallels-Parallels-Virtual-Platform.(none)>
2013-07-23 11:24:53 +0200

    corrected master

--- manual-cluster/install-master-only.sh
+++ manual-cluster/install-master-only.sh
@@ -1,5 +1,9 @@
-
-VERSION=hadoop-1.0.4
+#!/bin/bash
+VERSION=hadoop-1.1.2
+ssh-copy-id -i $HOME/.ssh/id_rsa.pub hduser@slave1
+ssh-copy-id -i $HOME/.ssh/id_rsa.pub hduser@slave2
+ssh-copy-id -i $HOME/.ssh/id_rsa.pub hduser@slave3
+ssh-copy-id -i $HOME/.ssh/id_rsa.pub hduser@slave4
 cd /usr/local/$VERSION/conf
 rm masters
 wget https://raw.github.com/ajehang/scripts/master/hadoop/conf/masters
