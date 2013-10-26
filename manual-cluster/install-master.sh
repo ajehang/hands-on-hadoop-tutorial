fe68c35cf931dae8d7a224875e23a833f427effe
Ali Imran Jehangiri <ali.jehangiri@gwdg.de>
2013-10-26 17:09:09 +0200

    Update install-master.sh

--- manual-cluster/install-master.sh
+++ manual-cluster/install-master.sh
@@ -6,11 +6,11 @@ apt-get update
 apt-get -y install openjdk-6-jdk
 
 # set hostnames
-echo "192.168.122.189 master.hbase.gwdg master" >> /etc/hosts
-echo "192.168.122.8   slave1.hbase.gwdg slave1" >> /etc/hosts
-echo "192.168.122.74  slave2.hbase.gwdg slave2" >> /etc/hosts
-#echo "10.1.0.130    slave3" >> /etc/hosts
-#echo "10.1.0.131    slave4" >> /etc/hosts
+echo "10.1.0.20 master.hbase.gwdg master" >> /etc/hosts
+echo "10.1.0.75   slave1.hbase.gwdg slave1" >> /etc/hosts
+echo "10.1.0.83  slave2.hbase.gwdg slave2" >> /etc/hosts
+echo "10.1.0.130    slave3" >> /etc/hosts
+echo "10.1.0.131    slave4" >> /etc/hosts
 
 #download hadoop, untar, put in /usr/local
 cd /tmp
