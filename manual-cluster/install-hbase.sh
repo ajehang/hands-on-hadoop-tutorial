560a2ecc657377bebda42cbd65945ad1d41698b7
Ali Imran Jehangiri <ali.jehangiri@gwdg.de>
2013-10-26 19:04:18 +0200

    Update install-hbase.sh

--- manual-cluster/install-hbase.sh
+++ manual-cluster/install-hbase.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 MIRROR=http://apache.osuosl.org/hbase/hbase-0.94.12/
 VERSION=hbase-0.94.12
-
+HADOOP_VERSION=1.2.1
 #download hadoop, untar, put in /usr/local
 cd /tmp
 wget "$MIRROR/$VERSION".tar.gz
@@ -17,6 +17,7 @@ rm "$VERSION".tar.gz
 #modify hadoop-env
 cd /usr/local/$VERSION/conf
 echo "export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk-amd64" >> hbase-env.sh
+echo "export HBASE_MANAGES_ZK=true" >>hbase-env.sh
 
 #get configuration files
 rm hbase-site.xml
@@ -31,11 +32,10 @@ chown hduser:hadoop $VERSION
 
 cd /usr/local/hbase/lib
 
-cp /usr/local/hadoop/$VERSION.jar .
-chmod +wx $VERSION.jar 
+cp /usr/local/hadoop/$HADOOP_VERSION.jar .
+chmod +wx $HADOOP_VERSION.jar 
 rm hadoop-core-1.0.4.jar
 
-echo "export HBASE_MANAGES_ZK=true" >>hbase-env.sh
 
 #cd /home/hduser/
 #wget https://github.com/ajehang/hands-on-hadoop-tutorial/raw/master/manual-cluster/conf/.bashrc
